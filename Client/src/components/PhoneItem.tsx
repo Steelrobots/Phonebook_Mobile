31c0634f1cafc5e77b8156e49e70ceb623b7b24a
Andri Priyadi <dripriyadi@gmail.com>
2024-02-15 16:02:32 +0700

    feat: item and search bar skeleton done

--- Client/src/components/PhoneItem.tsx
+++ Client/src/components/PhoneItem.tsx
@@ -3,26 +3,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { useState } from "react";
 import { StyleSheet, TouchableOpacity } from "react-native";
 
-export default function PhoneItem(){
+export default function PhoneItem() {
     const [isEdit, setIsEdit] = useState(false);
 
 
-    return(
+    return (
         <view>
             <view>
 
             </view>
             <view>
-            <text>Andri Priyadi</text>
-            <text>089695991757</text>
+                <text>Andri Priyadi</text>
+                <text>089695991757</text>
                 <view>
-                    <TouchableOpacity><FontAwesomeIcon icon={faPenToSquare}/></TouchableOpacity>
-                    <TouchableOpacity><FontAwesomeIcon icon={faTrashCan}/></TouchableOpacity>
+                    <TouchableOpacity><FontAwesomeIcon icon={faPenToSquare} /></TouchableOpacity>
+                    <TouchableOpacity><FontAwesomeIcon icon={faTrashCan} /></TouchableOpacity>
                 </view>
             </view>
         </view>
     )
 };
-const styles = StyleSheet.create({
-    
-})
--- Client/src/components/SearchBar.tsx
+++ Client/src/components/SearchBar.tsx
@@ -1,19 +1,37 @@
-import { faArrowUpAZ, faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import { faArrowDownAZ, faArrowUpAZ, faUserPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { TextInput, TouchableOpacity } from "react-native";
+import { KeyboardAvoidingView, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+
+
 
 export default function SearchBar(
     { sort, setSort, keyword, setKeyword }:
         {
             sort: string, setSort: React.Dispatch<React.SetStateAction<string>>,
-            keyword: string,setKeyword: React.Dispatch<React.SetStateAction<string>>
+            keyword: string, setKeyword: React.Dispatch<React.SetStateAction<string>>
         }) {
+
+
     return (
-        <view>
-            <TouchableOpacity><FontAwesomeIcon icon={faArrowUpAZ} /></TouchableOpacity>
-            <TextInput />
+        <KeyboardAvoidingView style={styles.container}  >
+            {sort === 'asc' ? (<TouchableOpacity onPress={() => { setSort('desc'); }}><FontAwesomeIcon icon={faArrowUpAZ} /></TouchableOpacity>) :
+                (<TouchableOpacity onPress={() => { setSort('asc'); }} ><FontAwesomeIcon icon={faArrowDownAZ} /></TouchableOpacity>)}
+            <view>
+                <TextInput value={keyword} onChangeText={text => setKeyword(text)} placeholder="Search" />
+            </view>
             <TouchableOpacity><FontAwesomeIcon icon={faUserPlus} /></TouchableOpacity>
 
-        </view>
+        </KeyboardAvoidingView>
     )
+    
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        position:'relative',
+        padding:5,
+    }
+})
